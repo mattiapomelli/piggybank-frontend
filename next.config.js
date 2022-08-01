b9013d316b9105c0ec8b186e2fc062089dcbca13
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-01 19:07:03 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    })
+
+    return config
+  },
+  ...nextConfig,
+}
