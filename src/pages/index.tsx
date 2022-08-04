bbe8ffde691276c5d5cae81a67150b1bd1529b79
twotimesgi <luigibardellagerbi@mail.com>
2022-08-04 19:57:01 +0200

    Tabella

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import type { NextPage } from 'next'
 
 import Container from '@components/Layout/Container'
 import DepositForm from '@components/Deposit/DepositForm'
-// import DepositsList from '@components/DepositsList'
+import DepositsList from '@components/Deposit/DepositsList'
 
 const Home: NextPage = () => {
   return (
     <Container className="py-6">
       <DepositForm />
-      {/* <DepositsList /> */}
+      <DepositsList /> 
     </Container>
   )
 }
