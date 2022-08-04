8861d1ec6b1fb7c8faa2e22cce6a75731ac70d11
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-04 19:50:52 +0200

    Implement deposit with approve

--- src/components/Deposit/DepositForm.tsx
+++ src/components/Deposit/DepositForm.tsx
@@ -1,4 +1,14 @@
+import { PIGGY_BANK_ADDRESS } from '@constants/addresses'
+import { CHAIN } from '@constants/chains'
+import { useTokenContractRead } from '@hooks/useContractRead'
+import {
+  usePiggyBankContractWrite,
+  useTokenContractWrite,
+} from '@hooks/useContractWriteAndWait'
+import { BigNumber, ethers } from 'ethers'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { useAccount } from 'wagmi'
 import Button from '../UI/Button'
 
 import InputField from '../UI/Input'
@@ -6,7 +16,7 @@ import InputField from '../UI/Input'
 interface DepositInputs {
   name: string
   amount: string
-  widthdrawalDate: string
+  withdrawalDate: string
 }
 
 const DepositForm = ({}) => {
@@ -14,42 +24,115 @@ const DepositForm = ({}) => {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
+    reset,
   } = useForm<DepositInputs>()
+  const { address } = useAccount()
+  const [needsApprove, setNeedsApprove] = useState(false)
 
-  const onSubmit = handleSubmit((data) => console.log(data))
+  const { data: allowance, refetch } = useTokenContractRead<BigNumber>({
+    functionName: 'allowance',
+    args: [address, PIGGY_BANK_ADDRESS[CHAIN.id]],
+  })
+
+  const { write: approve, isLoading: loadingApprove } = useTokenContractWrite({
+    functionName: 'approve',
+    onSuccess() {
+      setNeedsApprove(false)
+      refetch()
+    },
+  })
+
+  const { write: deposit, isLoading: loadingDeposit } =
+    usePiggyBankContractWrite({
+      functionName: 'deposit',
+      onSuccess() {
+        reset()
+      },
+    })
+
+  const onApprove = () => {
+    approve({
+      args: [
+        PIGGY_BANK_ADDRESS[CHAIN.id],
+        ethers.utils.parseEther(getValues('amount')),
+      ],
+    })
+  }
+
+  const onSubmit = handleSubmit((data) => {
+    const { name, amount, withdrawalDate } = data
+    const depositAmount = ethers.utils.parseEther(amount)
+
+    if (allowance && allowance.lt(depositAmount)) {
+      setNeedsApprove(true)
+      return
+    }
+
+    deposit({
+      args: [name, depositAmount, new Date(withdrawalDate).getTime()],
+    })
+  })
 
   return (
-    <form className="flex flex-col gap-4" onSubmit={onSubmit}>
-      <h2 className="text-2xl font-bold">Deposit</h2>
-      <InputField
-        label="Name"
-        placeholder="Your deposit name"
-        id="name"
-        fullWidth
-        error={errors.name?.message}
-        {...register('name', { required: 'Name is required' })}
-      />
-      <InputField
-        label="Amount"
-        placeholder="Enter amount"
-        id="amount"
-        type="number"
-        fullWidth
-        error={errors.amount?.message}
-        {...register('amount', { required: 'Amount is required' })}
-      />
-      <InputField
-        label="Withdrawal date"
-        id="withdrawalDate"
-        type="Date"
-        fullWidth
-        error={errors.withdrawalDate?.message}
-        {...register('withdrawalDate', { required: 'Withdrawal date is required' })}
-      />
-      <Button type="submit" className="mt-2">
-        Deposit
-      </Button>
-    </form>
+    <>
+      <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+        <h2 className="text-2xl font-bold">Deposit</h2>
+        <InputField
+          label="Name"
+          placeholder="Your deposit name"
+          id="name"
+          fullWidth
+          error={errors.name?.message}
+          {...register('name', { required: 'Name is required' })}
+        />
+        <InputField
+          label="Amount"
+          placeholder="Enter amount"
+          id="amount"
+          type="number"
+          fullWidth
+          error={errors.amount?.message}
+          {...register('amount', { required: 'Amount is required' })}
+        />
+        <InputField
+          label="Withdrawal date"
+          id="withdrawalDate"
+          type="Date"
+          fullWidth
+          error={errors.withdrawalDate?.message}
+          {...register('withdrawalDate', {
+            required: 'Withdrawal date is required',
+          })}
+        />
+        <Button
+          type="submit"
+          className="mt-2"
+          loading={loadingDeposit}
+          disabled={needsApprove}
+        >
+          Deposit
+        </Button>
+      </form>
+      {needsApprove && (
+        <div className="bg-[#FDEDD4] rounded-xl mt-8 p-6">
+          <p>
+            Your allowance is insufficient. You first need to approve the
+            spending of your tokens in order to deposit
+          </p>
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              className="mt-2"
+              onClick={onApprove}
+              loading={loadingApprove}
+            >
+              Approve
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
